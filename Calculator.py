fe57be8f9a2bf83795f40b175cb3fa0425252db8
kp268 <15898380+kp268@users.noreply.github.com>
2021-06-25 14:38:52 -0500

    calc functions

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,21 @@
 
 
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+
+def addition(a, b):
+    c = a + b
+    return c
+
+
+def mean(data):
+    mean = data
+    return mean
+
+
 class Calculator:
     result = 0
